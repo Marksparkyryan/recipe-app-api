97ae765f1000a16e8c738886e525c66125d0c0c8
Mark Ryan <markryandeveloper@gmail.com>
2020-03-24 22:41:53 -0400

    Correcting syntax line 448 in test_user_api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -118,11 +118,32 @@ class PrivateUsersApiTests(TestCase):
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
-    def test_retrieve _profile_success(self):
+    def test_retrieve_profile_success(self):
         """Test retrieving profile for logged in user"""
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        sel.assertEqual(res.data, {
-            'email': self.user.name
+        self.assertEqual(res.data, {
+            'email': self.user.email,
+            'name': self.user.name
         })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the use rprofile for authenticated uder"""
+        payload = {
+            'name': 'new name',
+            'password': 'newpasword123'
+        }
+        res = self.client.patch(ME_URL, payload)
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(
+            payload['password']), status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
