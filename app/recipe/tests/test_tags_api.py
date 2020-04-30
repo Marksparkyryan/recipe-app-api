59d4fb66adb194e70d58322dadbd7b53caceeb3b
Mark Ryan <markryandeveloper@gmail.com>
2020-04-29 20:10:24 -0400

    added ingredient list feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -62,7 +62,7 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
-    
+
     def test_create_tag_successful(self):
         """Test creating a new tag"""
         payload = {'name': 'Test Tag'}
@@ -74,11 +74,10 @@ class PrivateTagsApiTests(TestCase):
         ).exists()
 
         self.assertTrue(exists)
-    
+
     def test_create_tag_invalid(self):
         """Test creating a new tag with invalid payload"""
         payload = {'name': ''}
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
